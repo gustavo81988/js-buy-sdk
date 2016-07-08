7fcdd45c55003f33851b2195f253b1d6ec5f7d91
Tim Anema <tim.anema@shopify.com>
2016-07-08 14:56:56 -0400

    refactored so that console methods are not lazily defined

--- tests/unit/api/logger-test.js
+++ tests/unit/api/logger-test.js
@@ -1,47 +1,46 @@
-/* eslint no-new: 0 */
-
+/* eslint no-new: 0, no-console: 0,  */
 import { module, test } from 'qunit';
 import logger from 'shopify-buy/logger';
+import { wrapConsole } from 'shopify-buy/logger';
 
 const testOutput = 'test output';
 
 module('Unit | Logger');
 
-['debug', 'info', 'warn', 'error'].forEach(method => {
-  test(`it should wrap ${method}`, function (assert) {
-    assert.expect(2);
-
-    /* eslint-disable no-console */
-    const oldLog = console[method];
-
-    console[method] = function () {
-      assert.equal(arguments[0], '[JS-BUY-SDK]: ', `console.${method} should be tagged`);
-      assert.equal(arguments[1], testOutput, `console.${method} should be wrapped`);
-    };
-
-    logger[method](testOutput);
-    console[method] = oldLog;
-    /* eslint-enable no-console */
-  });
-});
-
-test('it should call console.log if the method is not defined', function (assert) {
-  assert.expect(1);
-
+test('it should wrap a valid console method', function (assert) {
+  assert.expect(2);
   /* eslint-disable no-console */
   const oldLog = console.log;
-  const oldWarn = console.warn;
 
   console.log = function () {
-    assert.equal(arguments[1], testOutput, 'console.log should be called');
+    assert.equal(arguments[0], '[JS-BUY-SDK]: ', 'console.log should be tagged');
+    assert.equal(arguments[1], testOutput, 'console.log should be wrapped');
   };
-  /* eslint-disable no-undefined */
-  console.warn = undefined;
-  /* eslint-enable no-undefined */
 
-  logger.warn(testOutput);
-
+  wrapConsole('log')(testOutput);
   console.log = oldLog;
-  console.warn = oldWarn;
   /* eslint-enable no-console */
+});
+
+test('it should fallback to console.log if the method is not defined', function (assert) {
+  assert.expect(2);
+  /* eslint-disable no-console */
+  const oldLog = console.log;
+
+  console.log = function () {
+    assert.equal(arguments[0], '[JS-BUY-SDK]: ', 'console.log should be tagged');
+    assert.equal(arguments[1], testOutput, 'console.log should be wrapped');
+  };
+
+  wrapConsole('output')(testOutput);
+  console.log = oldLog;
+  /* eslint-enable no-console */
+});
+
+['debug', 'info', 'warn', 'error'].forEach(method => {
+  test(`it should wrap ${method}`, function (assert) {
+    assert.expect(1);
+    logger[method](testOutput);
+    assert.ok(true, `${method} should not throw an error`);
+  });
 });

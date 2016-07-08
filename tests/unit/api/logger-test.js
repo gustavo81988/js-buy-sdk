cca346b74b74373b0a9dde7879230c90afb07b9a
Tim Anema <tim.anema@shopify.com>
2016-07-08 15:04:29 -0400

    moved logger mocks to before and after hooks

--- tests/unit/api/logger-test.js
+++ tests/unit/api/logger-test.js
@@ -4,37 +4,33 @@ import logger from 'shopify-buy/logger';
 import { wrapConsole } from 'shopify-buy/logger';
 
 const testOutput = 'test output';
+let oldLog;
 
-module('Unit | Logger');
+module('Unit | Logger', {
+  before() {
+    oldLog = console.log;
+  },
+  after() {
+    console.log = oldLog;
+  }
+});
 
 test('it should wrap a valid console method', function (assert) {
   assert.expect(2);
-  /* eslint-disable no-console */
-  const oldLog = console.log;
-
   console.log = function () {
     assert.equal(arguments[0], '[JS-BUY-SDK]: ', 'console.log should be tagged');
     assert.equal(arguments[1], testOutput, 'console.log should be wrapped');
   };
-
   wrapConsole('log')(testOutput);
-  console.log = oldLog;
-  /* eslint-enable no-console */
 });
 
 test('it should fallback to console.log if the method is not defined', function (assert) {
   assert.expect(2);
-  /* eslint-disable no-console */
-  const oldLog = console.log;
-
   console.log = function () {
     assert.equal(arguments[0], '[JS-BUY-SDK]: ', 'console.log should be tagged');
     assert.equal(arguments[1], testOutput, 'console.log should be wrapped');
   };
-
   wrapConsole('output')(testOutput);
-  console.log = oldLog;
-  /* eslint-enable no-console */
 });
 
 ['debug', 'info', 'warn', 'error'].forEach(method => {
